6a23a2aa3d61a26d8c93987c0875c1b5ddb31ef6
afzaal wasim <80407489+afzaalwasm@users.noreply.github.com>
2021-04-27 02:42:35 +0500

    27/04/2021/2:42

--- app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
@@ -1,8 +1,10 @@
 package com.androiddevs.mvvmnewsapp.repository
 
+import com.androiddevs.mvvmnewsapp.api.RetrofitInstanc
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
 
 class NewsRepository(
     val db: ArticleDatabase
 ) {
+    suspend fun  getBreakingNews(countryCode: String,pageNumber:Int )= RetrofitInstanc.api.getBreakingNews(countryCode, pageNumber)
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/NewsViewModel.kt
@@ -1,10 +1,32 @@
 package com.androiddevs.mvvmnewsapp.ui.fragments
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.androiddevs.mvvmnewsapp.models.NewsResponse
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+import com.androiddevs.mvvmnewsapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newRepository: NewsRepository
-) : ViewModel (){
+) : ViewModel() {
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingPage = 1
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val resource = newRepository.getBreakingNews(countryCode, breakingPage)
+    }
+
+    private fun handlerBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let {
+                return Resource.Success(it)
+            }
+        }
+        return Resource.Error(response.message())
+
+    }
 }
